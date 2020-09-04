be558bea838244b7c64d5d39bd5829b00e1bfca3
yoni-lab <69520415+yoni-lab@users.noreply.github.com>
2020-09-03 21:54:40 -0400

    Add files via upload
    Ejercicio 4 ("Alhandal") de Agoritmos Corregido y/o Modificado ¡¡¡¡¡¡¡

---
+++ alhadalCorregido/albahal.java
@@ -0,0 +1,22 @@
+
+public class albahal{
+    public static void main(String args[]){   
+    int monedaA=1;
+    int pesoMonedaA=6;
+    int monedaB=5;
+    int pesoMonedaB=8;
+    int monedaC=10;
+    int pesoMonedaC=10;
+    int juanMoneda=28;
+    int pesoMonedaJuan=68;
+    
+    String nom = "Cantidad de monedaA es : ";
+    String nom2 = "Cantidad de monedaC es : ";
+    
+    monedaA = 8;
+    monedaC = 2;
+    System.out.println(nom + monedaA);
+    System.out.println( nom2 + monedaC);
+    
+}
+}
